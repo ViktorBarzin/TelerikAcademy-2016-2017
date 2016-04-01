6f346c4620db56385e712166c1d4bfc11aa10d6a
EdinPriqtel <vbarzin@gmail.com>
2016-04-01 19:18:57 +0300

    intro to programming

---
+++ CSharp-Fundamentals/IntroToProgramming/15.Age/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('.');
+            DateTime birthday = new DateTime(int.Parse(input[2]), int.Parse(input[0]), int.Parse(input[1]));
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - birthday.Year;
+
+            
+            if (age > 0)
+            {
+                Console.WriteLine(age-1);
+                Console.WriteLine(age + 9);
+            }
+            else
+            {
+                Console.WriteLine(age);
+                Console.WriteLine(age + 10);
+            }
+        }
+    }
+}
