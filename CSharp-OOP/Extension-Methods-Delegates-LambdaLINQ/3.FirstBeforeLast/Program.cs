485d614c958b1ed05c10a30fe1f9b4f904ec8540
ViktorBarzin <vbarzin@gmail.com>
2016-06-16 16:34:14 +0300

    sorted students descending

--- CSharp-OOP/Extension-Methods-Delegates-LambdaLINQ/3.FirstBeforeLast/Program.cs
+++ CSharp-OOP/Extension-Methods-Delegates-LambdaLINQ/3.FirstBeforeLast/Program.cs
@@ -13,7 +13,7 @@ namespace _3.FirstBeforeLast
             List<Student> students = new List<Student>();
             var studentsWhoseFirstNameIsBeforeTheirLast = students.Where(x => String.Compare(x.FirstName, x.LastName, StringComparison.Ordinal) < 0).ToList();
             var studentsBetween18And24 = students.Where(x => x.Age >= 18 && x.Age < 24).ToList();
-            var sortedStudents = students.OrderBy(x => x.FirstName).ThenBy(x => x.LastName).ToList();
+            var sortedStudents = students.OrderByDescending(x => x.FirstName).ThenBy(x => x.LastName).ToList();
         }
     }
 }
